9516534131e44ebae76071b26a27797b189cf721
Piterden <efremov.a.denis@gmail.com>
2019-03-02 06:10:51 +0300

    Fixed eslint errors

--- src/mixins/GridMixin.js
+++ src/mixins/GridMixin.js
@@ -1,6 +1,6 @@
 export default {
   methods: {
-    getNextId(id) {
+    getNextId (id) {
       const match = id.match(/(\d+):(\d+)/)
       const last = this.active.word[this.active.word.length - 1]
       const next = Number(match[Number(this.active.vertical) + 1]) + 1
@@ -9,12 +9,12 @@ export default {
         return false
       }
 
-      return this.active.vertical ?
-        `${match[1]}:${next}` :
-        `${next}:${match[2]}`
+      return this.active.vertical
+        ? `${match[1]}:${next}`
+        : `${next}:${match[2]}`
     },
 
-    getPrevId(id) {
+    getPrevId (id) {
       const match = id.match(/(\d+):(\d+)/)
       const first = this.active.word[0]
       const prev = Number(match[Number(this.active.vertical) + 1]) - 1
@@ -23,9 +23,9 @@ export default {
         return false
       }
 
-      return this.active.vertical ?
-        `${match[1]}:${prev}` :
-        `${prev}:${match[2]}`
+      return this.active.vertical
+        ? `${match[1]}:${prev}`
+        : `${prev}:${match[2]}`
     },
   },
 }
