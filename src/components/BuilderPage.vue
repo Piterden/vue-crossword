91063d703b4147d07a5c104bc12a93376b9e9d5b
Piterden <efremov.a.denis@gmail.com>
2019-06-18 08:12:35 +0300

    Fix 2

--- src/components/BuilderPage.vue
+++ src/components/BuilderPage.vue
@@ -182,6 +182,7 @@ export default {
     gridName: '',
     prevWord: null,
     filledWords: [],
+    removeCount: 0,
     suggestions: [],
     crosswordId: null,
     focusedCell: '0:0',
@@ -384,14 +385,14 @@ export default {
         }))
     },
 
-    sortedByCounts () {
+    sortedCounts () {
       return Array.from(this.suggestionCounts)
         .sort((a, b) => a.count - b.count)
         .filter((obj) => obj && obj.count > 0 && obj.query.includes('_'))
     },
 
     nextQuery () {
-      return this.sortedByCounts[0] && this.sortedByCounts[0].query
+      return this.sortedCounts[0] && this.sortedCounts[0].query
     },
 
     queries () {
@@ -588,7 +589,15 @@ export default {
         })
 
       if (suggestionCounts.some(({ count }) => count === 0)) {
+        // eslint-disable-next-line no-magic-numbers
+        if (this.removeCount === 0 || this.removeCount > 20) {
+          this.prevWord = this.filledWords.pop()
+          this.removeCount = 0
+        }
+
         this.removeWord(this.prevWord)
+        this.removeCount += 1
+
         return this.autoFill()
       }
 
@@ -711,8 +720,6 @@ export default {
     },
 
     pasteWord ({ word: { word }, x, y, isVertical }) {
-      this.prevWord = { x, y, isVertical, word: { word } }
-
       Array.from(word).forEach((letter, index) => {
         const key = isVertical ? `${x}:${y + index}` : `${x + index}:${y}`
 
